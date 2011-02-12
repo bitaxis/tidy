2af3744c4cd14b3aa8a5540648a216d8f93f6f82
Nathan Brazil <nb@bitaxis.com>
2011-02-12 01:54:59 -0800

    Update for Rails 3 compatibility.

--- lib/tidy.rb
+++ lib/tidy.rb
@@ -1,78 +1,58 @@
-module ActionController
-
-  class Base
-
-    # Chain the render_for_file method so that we can capture the view template being rendered
-
-    def render_for_file_with_capture(template, status = nil, layout = nil, locals = {})
-      @rendered_template = template.path_without_format_and_extension
-      render_for_file_without_capture(template, status, layout, locals)
-    end
-
-    alias_method_chain :render_for_file, :capture
-
-  end
-
-end
-
 module ActionView
-
-  module Partials
-
-    # Chain the render_partial method so that we can capture all he partials being rendered as part of a view
-
-    def render_partial_with_capture(options = {})
-      @rendered_partials ||= []
-      @rendered_partials << options[:partial] unless @rendered_partials.include?(options[:partial])
-      render_partial_without_capture(options)
-    end
-
-    alias_method_chain :render_partial, :capture
-
-  end
-
-end
-
-module ActionView
-
+  
   class Base
-
+    
     def tidy_javascripts
-      @javascripts ||= []
+      @tidy_javascripts ||= []
       tidy_places.each do |javascript|
-        @javascripts << javascript if File.exists? "#{Dir.pwd}/public/javascripts/#{javascript}.js"
+        @tidy_javascripts << javascript if File.exists? "#{Dir.pwd}/public/javascripts/#{javascript}.js"
       end
-      @javascripts
+      @tidy_javascripts
     end
 
     def tidy_stylesheets
-      @stylesheets ||= []
+      @tidy_stylesheets ||= []
       tidy_places.each do |stylesheet|
-        @stylesheets << stylesheet if File.exists? "#{Dir.pwd}/public/stylesheets/#{stylesheet}.css"
+        @tidy_stylesheets << stylesheet if File.exists? "#{Dir.pwd}/public/stylesheets/#{stylesheet}.css"
       end
-      @stylesheets
+      @tidy_stylesheets
     end
-
-  private
-
-    def rendered_view_and_template
-      [template.name, "application", "#{controller.controller_path}/_controller", "#{@rendered_template}"]
-    end
-
-    def rendered_partials
-      return [] unless @rendered_partials
-      @rendered_partials.collect do |partial|
-        parts = partial.split(Regexp.new(File::SEPARATOR))
-        parts[-1].insert(0, "_")
-        parts.insert(0, controller_name) if parts.length < 2
-        parts.join(File::SEPARATOR)
-      end
-    end
-
+    
+    private
+    
     def tidy_places
-      rendered_view_and_template + rendered_partials
+      unless @tidy_places
+        @tidy_places = ["#{controller.controller_path}/_controller", @tidy_template]
+        @tidy_places.insert(0, @tidy_layout) if @tidy_layout
+        @tidy_places << @tidy_partials if @tidy_partials
+      end
+      @tidy_places
     end
+    
+  end
 
+  module Partials
+    
+    def _render_partial_with_capture(options, &block)
+      @tidy_partials ||= []
+      @tidy_partials << options[:partial] unless @tidy_partials.include?(options[:partial])
+      _render_partial_without_capture(options, &block)
+    end
+    
+    alias_method_chain :_render_partial, :capture
+    
+  end
+  
+  module Rendering
+    
+    def _render_template_with_capture(template, layout = nil, options = {})
+      @tidy_template = template.virtual_path
+      @tidy_layout = layout
+      _render_template_without_capture(template, layout, options)
+    end
+    
+    alias_method_chain :_render_template, :capture 
+    
   end
 
 end
